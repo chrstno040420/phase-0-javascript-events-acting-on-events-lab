6828a175a23c4fbd5225e6383e5cc31a82603de1
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-06-08 14:07:19 -0500

    commit tree fa24029bfc561590011370c4693dd88107f76439

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+//dodger.style.left = "360px";
+
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+function moveDodgerRight(){
+    let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 20);
+
+    if(left > 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(c){
+    if (c.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
